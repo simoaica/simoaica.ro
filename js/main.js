ac3ba5177e43b839b70324cd2e5f213efbbcc0e1
Dan <simoaica@yahoo.com>
2017-03-25 17:33:45 +0200

    finishing static city weather report

--- js/main.js
+++ js/main.js
@@ -2,6 +2,31 @@
 
 $(document).ready(function() {
 
+  // $( "#orase" ).on('change', function(){
+  //   oras=$( "#orase" ).val();
+  //   vremea(oras);
+  // });
+  vremea("bucuresti");
+
+  function vremea(oras){
+    $.simpleWeather({
+      location: oras,
+      woeid: '',
+      unit: 'c',
+      success: function(weather) {
+        html = '<h2><i class="icon-'+weather.code+'"></i> '+weather.temp+'&deg;'+weather.units.temp+'</h2>';
+        html += '<ul><li>'+weather.city+', '+weather.region+'</li>'; //+', '+weather.region
+        html += '<li class="currently">'+weather.currently+'</li>';
+        html += '</ul>';
+
+        $("#weather").html(html);
+      },
+      error: function(error) {
+        $("#weather").html('<p>'+error+'</p>');
+      }
+    });
+  }
+
   // Setari particule
 
   particlesJS('particule',
